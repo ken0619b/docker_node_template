9aeeaac87b5d0297ce9980660ff223bcc62ef767
Kenichi Ukai <[ken151102@gmail.com]>
2019-01-02 17:15:10 +0900

    add radium

--- src/test_react/src/App.js
+++ src/test_react/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -65,7 +66,11 @@ class App extends Component {
       font: 'inherit',
       border: '1x solid blue',
       padding: '8px',
-      cursol: 'pointer'
+      cursol: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -91,7 +96,11 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red'
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -115,4 +124,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
